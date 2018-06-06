ab9d82088df91d660710d5f431c3db1568592494
Boris Glass <boris@glass.li>
2018-06-06 17:59:54 +0200

    parent-child relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+
+  belongs_to :parent, class_name: "Item"
+  has_many :children, class_name: "Item", foreign_key: "parent_id"
+
   has_many_attached :images
   # Note that implicit association has a plural form in this case
   scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
