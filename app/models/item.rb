943e5f2c47cf7c46a697ed8ede05ef2b30547508
Boris Glass <boris@glass.li>
2018-06-08 15:56:34 +0200

    hash store, form fields. ??

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,21 @@ class Item < ApplicationRecord
   has_many_attached :images
   # Note that implicit association has a plural form in this case
   scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
+
+  store :data
+
+  def data_hash
+    self.data.collect{|k,v| [k,v]}
+  end
+
+  def data_hash=(param_hash)
+    # need to ensure deleted values from form don't persist
+    self.data.clear
+    param_hash.each do |name, value|
+      self.data[name.to_sym] = value
+    end
+  end
+
+
+
 end
